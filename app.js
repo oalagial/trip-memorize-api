a2a0ec9d78dae786e8808f3fa18c971f5acf9af0
orestis.alagialoglou <orestis.alagialoglou@marinetraffic.com>
2023-04-01 18:48:52 +0300

    Add endpoint for uploading images

--- app.js
+++ app.js
@@ -29,6 +29,56 @@ app.use(function (req, res, next) {
   next();
 });
 
+// Set up storage location and file name
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "Images/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${file.originalname}`);
+  },
+});
+
+// Set up multer middleware with storage and file type limits
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5, // limit file size to 5MB
+    files: 1, // limit number of files to 1
+  },
+  fileFilter: (req, file, cb) => {
+    console.log("upload", file);
+
+    const fileTypes = /jpeg|jpg|png|gif/; // allowed file types
+    const extname = fileTypes.test(
+      path.extname(file.originalname).toLowerCase()
+    );
+    const mimetype = fileTypes.test(file.mimetype);
+    if (extname && mimetype) {
+      return cb(null, true);
+    } else {
+      cb(new Error("Invalid file type"));
+    }
+  },
+}).single("image"); // the name of the file input field in the HTML form
+
+// Route to handle image uploads
+app.post("/upload", (req, res) => {
+  console.log("upload");
+  upload(req, res, (err) => {
+    if (err) {
+      console.log("err.message", err.message);
+      res.status(400).send(err.message);
+    } else {
+      if (!req.file) {
+        res.status(400).send("No file selected");
+      } else {
+        res.send(`File uploaded successfully: ${req.file.filename}`);
+      }
+    }
+  });
+});
+
 app.get("/api", (req, res) => {
   res.send("Hello, world!");
 });
